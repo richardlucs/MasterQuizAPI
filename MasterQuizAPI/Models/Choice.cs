7e62726f69a8556a7caf27c77942da42ddfdf8b3
unknown <richardlucasoliveira@gmail.com>
2022-05-16 19:43:33 -0300

    Models 2.0

---
+++ MasterQuizAPI/Models/Choice.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterQuizAPI.Models
+{
+    public class Choice
+    {
+        public int ChoiceId { get; set; }
+        public int QuestionId { get; set; }
+        public string? ChoiceText { get; set; }
+        public bool IsTrue { get; set; }
+    }
+}
--- MasterQuizAPI/Models/Questions.cs
+++ MasterQuizAPI/Models/Questions.cs
@@ -1,13 +1,5 @@
 ﻿namespace MasterQuizAPI.Models
 {
-    public class Choice
-    {
-        public int ChoiceId { get; set; }
-        public int QuestId { get; set; }
-        public string? ChoiceText { get; set; }
-        public bool? IsTrue { get; set; }
-    }
-
     public class Questions
     {
         public int QuestId { get; set; }    
